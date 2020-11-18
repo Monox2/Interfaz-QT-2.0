cefc6f1050d871aa144327e4091a0e29a8538470
Ramón García Espejo <ramon.garcia5800@alumnos.udg.mx>
2020-11-18 09:40:50 -0600

    Agregue la ventana de dibujo

---
+++ particula/particula.py
@@ -0,0 +1,83 @@
+from .algoritmos import distancia_euclidiana
+
+class Particula:
+     def __init__(self, 
+                id = 0, origen_x = 0, origen_y = 0,
+                destino_x = 0, destino_y = 0 , velocidad = 0,
+                red = 0 , green = 0 , blue = 0 , distancia = 0.0):
+            self.__id = id
+            self.__origen_x = origen_x 
+            self.__origen_y = origen_y
+            self.__destino_x = destino_x
+            self.__destino_y = destino_y
+            self.__velocidad = velocidad
+            self.__red = red
+            self.__green = green
+            self.__blue = blue
+            self.distancia = distancia_euclidiana(origen_x, destino_x, origen_y, destino_y)
+     
+     def __str__(self):
+         return(
+             'Id: ' + str(self.__id) + '\n' +
+             'Origen_x: ' + str(self.__origen_x) + '\n' +
+             'Origen_y: ' + str(self.__origen_y) + '\n' +
+             'Destino_x: ' + str(self.__destino_x) + '\n' +
+             'Destino_y: ' + str(self.__destino_y) + '\n' +
+             'Velocidad: ' + str(self.__velocidad) + '\n' +
+             'Red: ' + str(self.__red) + '\n' +
+             'Green: ' + str(self.__green) + '\n' +
+             'Blue: ' + str(self.__blue) + '\n' +
+             'Distancia: ' + str(self.distancia) + '\n'
+         )
+
+     @property
+     def id(self):
+         return self.__id
+
+     @property 
+     def origen_x(self):
+         return self.__origen_x
+
+     @property
+     def origen_y(self):
+         return self.__origen_y
+
+     @property
+     def destino_x(self):
+         return self.__destino_x
+    
+     @property
+     def destino_y(self):
+         return self.__destino_y
+    
+     @property
+     def velocidad(self):
+         return self.__velocidad
+
+     @property
+     def red(self):
+         return self.__red
+
+     @property
+     def green(self):
+         return self.__green
+
+     @property
+     def blue(self):
+         return self.__blue
+
+     def to_dict(self):
+         return{
+             "id": self.__id,
+             "origen_x": self.__origen_x,
+             "origen_y": self.__origen_y,
+             "destino_x": self.__destino_x,
+             "destino_y": self.__destino_y,
+             "velocidad": self.__velocidad,
+             "red": self.__red,
+             "green": self.__green,
+             "blue": self.__blue
+         }
+# p01 = Particula(id= 1, origen_x= 7, origen_y= 4, destino_x= 5, destino_y= 1, velocidad= 100, red= 50, green= 75, blue= 100, distancia= distancia_euclidiana)
+# print(p01)
+   
---
+++ particula/particulas.py
@@ -0,0 +1,66 @@
+from .particula import Particula
+from .algoritmos import distancia_euclidiana
+import json
+
+class Particulas:
+    def __init__(self):
+        self.__particulas = []
+    
+    def agregar_final(self, particula:Particula):
+        self.__particulas.append(particula)
+
+    def agregar_inicio(self, particula:Particula):
+        self.__particulas.insert(0, particula)
+
+    def mostrar(self):
+        for particula in self.__particulas:
+            print(particula)
+
+    def __str__(self):
+        return "".join(
+            str(particula) + '\n' for particula in self.__particulas
+        ) 
+
+    def __len__(self):
+        return len(self.__particulas)
+
+    def __iter__(self):
+        self.cont = 0
+        
+        return self
+    
+    def __next__(self):
+        if self.cont < len(self.__particulas):
+            particula = self.__particulas[self.cont]
+            self.cont += 1
+            return particula
+        else:
+            raise StopIteration
+
+    def guardar(self, ubicacion):
+        try:
+            with open(ubicacion, 'w') as archivo:
+                lista = [particula.to_dict() for particula in self.__particulas]
+                print(lista)
+                json.dump(lista, archivo, indent = 5)
+            return 1
+        except:
+            return 0
+
+    def abrir(self, ubicacion):
+        try:
+            with open(ubicacion, 'r') as archivo:
+                lista = json.load(archivo)
+                self.__particulas = [Particula(**particula) for particula in lista]
+            return 1
+        except:
+            return 0       
+
+# p01 = Particula(id= 1, origen_x= 7, origen_y= 4, destino_x= 5, destino_y= 1, velocidad= 100, red= 50, green= 75, blue= 100, distancia= distancia_euclidiana)
+# p02 = Particula(id= 2, origen_x= 500, origen_y= 200, destino_x= 500, destino_y= 200, velocidad= 100, red= 50, green= 75, blue= 100, distancia= distancia_euclidiana)
+# particulas = Particulas()
+# particulas.agregar_final(p01)
+# particulas.agregar_inicio(p02)
+# particulas.agregar_inicio(p01)
+# particulas.mostrar()
+
